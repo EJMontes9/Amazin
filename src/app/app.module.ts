0a6774f02f08dd9dcb6bf27196cce99516754d2a
IRamos24 <harijan-14@hotmail.com>
2022-07-05 23:16:47 -0500

    Creacion FAQ :100:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './Components/slider/slider.component';
+import { FAQComponent } from './Components/faq/faq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    FAQComponent
   ],
   imports: [
     BrowserModule,

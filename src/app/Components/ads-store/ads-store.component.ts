2c85fe0643f7db764b8882d85f7d6b5a5315b5d7
JoelAndres1998 <joelvillao@gmail.com>
2022-07-09 19:02:15 -0500

    Component-home
    Terminado

---
+++ src/app/Components/ads-store/ads-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ads-store',
+  templateUrl: './ads-store.component.html',
+  styleUrls: ['./ads-store.component.css']
+})
+export class AdsStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './Components/slider/slider.component';
+import { AdsStoreComponent } from './Components/ads-store/ads-store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    AdsStoreComponent,
   ],
   imports: [
     BrowserModule,

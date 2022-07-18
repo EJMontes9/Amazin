54e302fffa390a83f418be79b7d90ce47fcf3179
Erick Montes <erickmontes77.jm@gmail.com>
2022-07-18 17:43:47 -0500

    creación de mas productos

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   public active : boolean = false
+  NameEnterprise : string = 'Amazing';
   constructor() {}
   ngOnInit(): void {}
 

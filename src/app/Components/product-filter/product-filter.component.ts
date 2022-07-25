66c0b4241cb47b1fbbd65153c4633d18e350ec97
IRamos24 <harijan-14@hotmail.com>
2022-07-25 13:36:21 -0500

    Last product changes

--- src/app/Components/product-filter/product-filter.component.ts
+++ src/app/Components/product-filter/product-filter.component.ts
@@ -17,19 +17,76 @@ export class ProductFilterComponent implements OnInit {
     nav.classList.toggle('hidden')
   }
 
-
-  MenuFilterResponsive() : void{
-    const nav = ((document.querySelector('#Icon')) as HTMLElement)
-    nav.classList.toggle('hidden')
-  }
-
  ShowHideResponsive() : void{
     const nav = ((document.querySelector('#FilterSection')) as HTMLElement)
     nav.classList.toggle('hidden')
   }
   
+  HiddenColors() : void{
 
+    const nav = ((document.querySelector('#Colors')) as HTMLElement)
+    nav.classList.toggle('hidden')
+    const svg= ((document.querySelector('#ClShow')) as HTMLElement)
+    svg.classList.toggle('hidden')
+    const svg2= ((document.querySelector('#ClHidden')) as HTMLElement)
+    svg2.classList.toggle('hidden')
+    
+  }
 
+  Categories() : void{
+
+    const ct = ((document.querySelector('#Categories')) as HTMLElement)
+    ct.classList.toggle('hidden')
+    const ct_svg= ((document.querySelector('#CtShow')) as HTMLElement)
+    ct_svg.classList.toggle('hidden')
+    const ct_svg2= ((document.querySelector('#CtHidden')) as HTMLElement)
+    ct_svg2.classList.toggle('hidden')
+  }
+
+  Sizes() : void{
+      
+      const nav = ((document.querySelector('#Sizes')) as HTMLElement)
+      nav.classList.toggle('hidden')
+      const s_svg= ((document.querySelector('#SzShow')) as HTMLElement)
+      s_svg.classList.toggle('hidden')
+      const s_svg2= ((document.querySelector('#SzHidden')) as HTMLElement)
+      s_svg2.classList.toggle('hidden')
+
+    }
+
+    //Functions for the Responsive Menu
+
+    HiddenColorsR() : void{
+
+      const nav = ((document.querySelector('#CResponsive')) as HTMLElement)
+      nav.classList.toggle('hidden')
+      const svg= ((document.querySelector('#CSResponsive')) as HTMLElement)
+      svg.classList.toggle('hidden')
+      const svg2= ((document.querySelector('#CHResponsive')) as HTMLElement)
+      svg2.classList.toggle('hidden')
+      
+    }
+  
+    CategoriesR() : void{
+  
+      const ct = ((document.querySelector('#CtResponsive')) as HTMLElement)
+      ct.classList.toggle('hidden')
+      const ct_svg= ((document.querySelector('#CtSResponive')) as HTMLElement)
+      ct_svg.classList.toggle('hidden')
+      const ct_svg2= ((document.querySelector('#CtHResponsive')) as HTMLElement)
+      ct_svg2.classList.toggle('hidden')
+    }
+  
+    SizesR() : void{
+        
+        const nav = ((document.querySelector('#SResponsive')) as HTMLElement)
+        nav.classList.toggle('hidden')
+        const s_svg= ((document.querySelector('#SSResponive')) as HTMLElement)
+        s_svg.classList.toggle('hidden')
+        const s_svg2= ((document.querySelector('#SHResponsive')) as HTMLElement)
+        s_svg2.classList.toggle('hidden')
+  
+      }
 
 }
 

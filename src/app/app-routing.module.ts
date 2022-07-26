af648d955cfed6b2b0b13fe3631657d5fe3efac3
Guillermo Baquerizo <guillermo_bame@outlook.com>
2022-07-25 21:07:26 -0500

    First Delivery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { ContacUsComponent } from './Components/contac-us/contac-us.component';
 import { FAQComponent } from './Components/faq/faq.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { ProductFilterComponent } from './Components/product-filter/product-filter.component';
+import { PurchasingProcessComponent } from './Components/purchasing-process/purchasing-process.component';
 import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
@@ -11,7 +14,10 @@ const routes: Routes = [
   { path: 'Register', component: RegisterComponent },
   { path: 'Login', component: LoginComponent },
   { path: 'FAQ', component: FAQComponent },
-  { path: 'Contactanos', component: ContacUsComponent}
+  { path: 'Contactanos', component: ContacUsComponent },
+  { path: 'ProductLists', component: ProductFilterComponent },
+  { path: 'ProductOverviews', component: ProductDetailsComponent },
+  {path: 'Pagos', component: PurchasingProcessComponent},
 ];
 
 @NgModule({
